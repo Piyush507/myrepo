19bebe4092902d0b8d54521fe849b24ff4a781b6
Piyush507 <piyushbhandari422@gmail.com>
2025-02-10 12:58:16 +0530

    first cpp file

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+void bubblesort()
+//
+
+void insertionsort()
+
+{
+
+	//
+}
+
+
+
+void quicksort()
+{
+
+	//
+}
+void mergesort()
+{
+
+	//
+}
+
+int main()
+{
+	cout<<"hello world"<<endl;
+	cout<<"new line"<<endl;
+	return 0;
+}
